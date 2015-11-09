ccbc739a791fdc36fc0495235bc22223634d039f
nel215 <otomo.yuhei@gmail.com>
2015-11-09 23:09:36 +0900

    added filters store

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {EventEmitter2 as EventEmitter} from 'eventemitter2';
+
+class FilterStore extends EventEmitter {
+  constructor() {
+    super();
+    this.callbacks = [];
+  }
+  fetchAll() {
+    this.emit('fetched');
+  }
+};
+
+var filterStore = new FilterStore();
 
 var Filter = React.createClass({
   getInitialState: function() {
     return {
       filters: ['http://*/*']
     };
+  },
+  updateFilter: function() {
+    this.setState({filters: ['http://test/*']});
+  },
+  componentDidMount: function() {
+    filterStore.on('fetched', this.updateFilter);
+    filterStore.fetchAll();
+  },
+  componentWillUnmount: function() {
+    filterStore.off('fetched', this.updateFilter);
   },
   render: function() {
     var urls = this.state.filters.map((url)=> {
