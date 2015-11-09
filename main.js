22d201ca0a57976113eb25cd1bf661cb12a4a9b5
nel215 <otomo.yuhei@gmail.com>
2015-11-09 23:14:29 +0900

    added button to add url

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ import {EventEmitter2 as EventEmitter} from 'eventemitter2';
 class FilterStore extends EventEmitter {
   constructor() {
     super();
-    this.callbacks = [];
+    this.filters = [];
   }
   fetchAll() {
     this.emit('fetched');
+  }
+  add(url) {
+    this.filters.push(url);
+    this.emit('added');
+  }
+  get() {
+    return this.filters;
   }
 };
 
@@ -21,14 +28,19 @@ var Filter = React.createClass({
     };
   },
   updateFilter: function() {
-    this.setState({filters: ['http://test/*']});
+    this.setState({filters: filterStore.get()});
+  },
+  addUrl: function() {
+    filterStore.add('http://test/*');
   },
   componentDidMount: function() {
     filterStore.on('fetched', this.updateFilter);
+    filterStore.on('added', this.updateFilter);
     filterStore.fetchAll();
   },
   componentWillUnmount: function() {
     filterStore.off('fetched', this.updateFilter);
+    filterStore.on('added', this.updateFilter);
   },
   render: function() {
     var urls = this.state.filters.map((url)=> {
@@ -36,6 +48,7 @@ var Filter = React.createClass({
     });
     return (
       <div style={{height: '160px', width: '320px'}}>
+        <button onClick={this.addUrl}>add</button>
         {urls}
       </div>
     );
