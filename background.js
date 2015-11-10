211cc765236c82847665832c964fb7eb477f278e
nel215 <otomo.yuhei@gmail.com>
2015-11-10 22:48:12 +0900

    enabled to filter by keywords

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 var callback = function(req){
-  console.log(req);
+  chrome.storage.sync.get(['filters', 'target'], (items)=> {
+    var filters = items.filters || [];
+    for(var i=0; i<filters.length; i++) {
+      console.log(filters[i]);
+      if(req.url.indexOf(filters[i]) >= 0) {
+        console.log(req);
+      }
+    }
+  });
 };
 
 chrome.webRequest.onBeforeRequest.addListener(callback, {urls: ['<all_urls>']});
